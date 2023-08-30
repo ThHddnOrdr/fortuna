9b36ec6771fd810899539bb7969ddadb7341e070
QCPOLstakepool <qcpolstakepool@gmail.com>
2023-08-29 23:52:38 -0400

    Worker

--- multi-miner/miner.ts
+++ multi-miner/miner.ts
@@ -13,11 +13,10 @@ import {
   } from "https://deno.land/x/lucid@0.10.1/mod.ts";
 
 self.onmessage = async (e) => {
-    const {index, validatorHash, validatorAddress, kupoUrl, ogmiosUrl, network} = e;
+    const {index, validatorHash, validatorAddress, kupoUrl, ogmiosUrl, network} = e.data;
 
     console.log(`Worker ${index}: Starting...`);
-    self.log("test");
-    
+
     const provider = new Kupmios(kupoUrl, ogmiosUrl);
     const lucid = await Lucid.new(provider, network);
     lucid.selectWalletFromSeed(Deno.readTextFileSync("seed.txt"));
