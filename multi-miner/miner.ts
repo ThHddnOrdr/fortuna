4166ad5613515b768e9b75f817dc36d0979f6aad
QCPOLstakepool <qcpolstakepool@gmail.com>
2023-08-29 23:41:07 -0400

    Worker

--- multi-miner/miner.ts
+++ multi-miner/miner.ts
@@ -1,9 +1,9 @@
 self.onmessage = async (e) => {
     const {index, validatorHash, validatorAddress, lucid} = e;
 
-    console.log(`Worker ${index}: Starting...`)
+    console.log(`Worker ${index}: Starting...`);
 
     let validatorUTXOs = await lucid.utxosAt(validatorAddress);
 
     console.log(`Worker ${index}: ${validatorUTXOs}`)
-});
+};
